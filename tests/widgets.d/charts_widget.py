9f2cb408f2cfe0595e1a369a854ddcd7373ca03a
aa830 <84448225+aa830@users.noreply.github.com>
2024-02-13 10:19:04 +0800

    Delete tests directory

--- tests/widgets.d/charts_widget.py
+++
@@ -1,4 +0,0 @@
-from colab.widgets.widget_manager import WidgetManager
-from colab.accounts.widgets.collaboration_chart import CollaborationChart
-
-WidgetManager.register_widget('charts', CollaborationChart())
